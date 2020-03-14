a980d1d5d1269c4a5a8991589571e3dbd072c30e
Bao, Le-Dinh <ledinhbao@me.com>
2020-03-14 22:17:40 +0700

    Create simple json server to return customer data

--- backend/server.go
+++ backend/server.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+// Customer struct contains information about an customer
+type Customer struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
-	fmt.Println("Hello backend server")
+	app := gin.Default()
+
+	// TODO connect to sqlite3 database
+	// TODO Fetch customers data from database
+	app.GET("/customers", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"customers": Customer{ID: 1, Name: "Tran Huu Nghi"},
+		})
+	})
+
+	app.Run()
 }
