b83f06501e9aaa65570111460f8c38bd0d37bfe1
Bao, Le-Dinh <ledinhbao@me.com>
2020-03-17 23:22:14 +0700

    Add NGCTransaction and dummy data on server

--- backend/server.go
+++ backend/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -11,12 +12,44 @@ import (
 type Customer struct {
 	ID               uint   `json:"id"`
 	Name             string `json:"name" form:"name"`
-	NGCHolding       int    `json:"ngcholding"`
-	Money            int    `json:"money"`
+	NGCHolding       int    `json:"ngcholding" form:"ngcholding"`
+	Money            int    `json:"money" form:"money"`
 	TransactionCount int    `json:"transaction_count"`
 }
 
-var customers []Customer
+// NGCTransaction records a transaction between customer and the Capital
+type NGCTransaction struct {
+	ID         uint
+	CustomerID uint
+	Amount     int
+	Price      float32
+	Date       time.Time
+
+	// database fields
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+var customers = []Customer{
+	Customer{
+		ID:               1,
+		Name:             "Tran Huu Nghi",
+		NGCHolding:       16000,
+		Money:            160,
+		TransactionCount: 1,
+	},
+}
+var ngcTransactions = []NGCTransaction{
+	NGCTransaction{
+		ID:         1,
+		CustomerID: 1,
+		Amount:     16000,
+		Price:      160,
+		Date:       time.Date(2020, 01, 10, 8, 0, 00, 0, time.Local),
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	},
+}
 
 func main() {
 	app := gin.Default()
