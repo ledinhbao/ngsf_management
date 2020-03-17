0d234c300fff9aefba9c2af52a94863d333b5288
Bao, Le-Dinh <ledinhbao@me.com>
2020-03-17 23:22:31 +0700

    Add fields for CustomerCreate form

--- frontend/src/views/CustomerCreate.vue
+++ frontend/src/views/CustomerCreate.vue
@@ -1,9 +1,19 @@
 <template>
     <div class='customer-create'>
+        <h1>Thêm nhà đầu tư</h1>
         <form @submit.prevent="onSubmit">
             <div class="input-block">
                 <label for="customerName">Họ & Tên</label>
-                <input type='text' v-model="customer.name" />
+                <input type='text' id="customerName" v-model="customer.name" />
+            </div>
+            <H3>Giao dịch khởi tạo</H3>
+            <div class="input-block">
+               <label for="ngcholding">NGC mua vào</label>
+                <input type='text' id="ngcholding" v-model="customer.ngcholding" /> 
+            </div>
+            <div class="input-block">
+               <label for="money">Giá mua NGC</label>
+                <input type='text' id="money" v-model="customer.money" /> 
             </div>
             <input type="submit" value="Thêm nhà đầu tư" />
             
@@ -16,7 +26,9 @@ export default {
     data() {
         return {
             customer: {
-                name: ""
+                name: "",
+                ngcholding: 10000,
+                money: 100
             }
         }
     },
